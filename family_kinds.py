ba6488ba278843c5b4c89e0a97aa65e063b1f461
squidzyy <jorby2006@gmail.com>
2026-02-13 01:26:34 +0800

    Add files via upload

---
+++ family_kinds.py
@@ -0,0 +1,77 @@
+# families.py
+
+family_kinds = {
+    "arrays_basic": [
+        "max",
+        "count_even",
+        "sum",
+        "minmax",
+        "count_odd",
+        "average",
+        "count_positive"
+    ],
+
+    "arrays_nested": [
+        "pair_sum_count",
+        "duplicate_values",
+        "max_pair_product",
+        "count_increasing_triplets",
+        "sum_unique_values",
+        "zero_sum_subarrays",
+        "pairs_with_diff_k"
+    ],
+
+    "bool_checks": [
+        "is_sorted",
+        "all_equal",
+        "majority_positive",
+        "contains_negative",
+        "alternating_signs",
+        "all_even",
+        "has_duplicate"
+    ],
+
+    "functions": [
+        "sum_array_fn",
+        "count_greater_fn",
+        "product_array",
+        "max_element_fn",
+        "count_even_fn",
+        "reverse_array_fn",
+        "sum_squares_fn"
+    ],
+
+    "patterns": [
+        "rect",
+        "triangle_num",
+        "right_triangle",
+        "inverted_num_triangle",
+        "pyramid",
+        "diamond",
+        "hollow_square"
+    ],
+
+    "strings": [
+        "palindrome",
+        "vowel_count",
+        "reverse_string"
+    ],
+
+    "math_logic": [
+        "sum_digits",
+        "prime_check",
+        "fibonacci_n"
+    ],
+
+    "recursion": [
+        "factorial",
+        "sum_n",
+        "reverse_string"
+    ],
+
+    "stl_intro": [
+        "vector_sum",
+        "sort_vector",
+        "word_count"
+    ],
+}
